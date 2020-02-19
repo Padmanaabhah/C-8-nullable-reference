198e35e1b4eadef06f737434ba982487fe17b704
Padmanaabhah <padmanaabhah@gmail.com>
2020-02-19 19:44:49 +0530

    First commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿#nullable enable
+using System;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Person person = new Person(null);
+            Console.WriteLine(person.MyAddress.ZipCode); 
+        }
+    }
+}
+
+class Person
+{
+    public string? Name { get; set; }
+
+    public int Age { get; set; }
+
+    public Address MyAddress { get; set; }
+
+    public Person(Address address)
+    {
+        MyAddress = address;
+    }
+}
+
+class Address
+{
+    public string City { get; set; }
+
+    public string? ZipCode { get; set; }
+
+}
+
+
+#nullable restore
